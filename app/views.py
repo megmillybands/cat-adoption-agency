5bfe2c808d4e2078feb4f985bdde515a54616291
megmillybands <meganlynncramer@gmail.com>
2025-12-16 14:18:02 -0600

    idek what i did but i made changed so here they are

--- app/views.py
+++ app/views.py
@@ -2,5 +2,3 @@ from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 
-def base(request):
-    return render(request, "base.html")
--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -112,3 +113,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
